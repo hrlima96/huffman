8cbb2ed2a4402618bf70e6003e79d7d6bba1ddc2
Tener Desenvolvimento <rubydev@Teners-Mac-mini.local>
2015-05-04 18:20:05 -0300

    confusao

--- huffman.py
+++ huffman.py
@@ -1,4 +1,5 @@
 import operator
+from no import No
 
 alfabeto = "abcdefghijklmnopqrstuvwxyz"
      
@@ -7,6 +8,7 @@ class Huffman:
     contador_letras = {letra: 0 for letra in alfabeto}
     lista = []
     lista_final = []
+    lista_nos = []
      
     def gerar(self, texto):
     	for letra in texto:
@@ -19,13 +21,13 @@ class Huffman:
             if self.lista[i][1] != 0:
                 self.lista_final.append(self.lista[i])
 
-        while len(self.lista_final) > 1:
-            s0 = min(self.lista_final)
-            self.lista_final.pop(self.lista_final.index(min(self.lista_final)))
-            s1 = min(self.lista_final)
-            self.lista_final.pop(self.lista_final.index(min(self.lista_final)))
-            print self.lista_final
-
+        while len(self.lista_final) > 2:
+            s0 = self.lista_final.pop(0)
+            s1 = self.lista_final.pop(1)
+            x = No(s0[1] + s1[1])
+            x.set_filho0(s0)
+            x.set_filho1(s1)
+            print x
 
      
      
